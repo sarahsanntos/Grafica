2c9533390dd74ba6b1bba4bfe0ac3fc91b053605
Marcos Barbosa <bob.barbosa2@gmail.com>
2015-01-07 16:38:27 -0300

    Update conn.php

--- config/conn.php
+++ config/conn.php
@@ -1,12 +1,20 @@
 <?php
+header('Content-Type: text/html; charset=UTF-8');
+
 //conecta o banco MySql
+$banco_dados="u208675905_chat";
+$user_db="u208675905_sarah";
+$senha_db="chatdb";
+$hostname="mysql.hostinger.com.br";
 
-$banco_dados="";
-$user_db="";
-$senha_db="";
-$hostname="";
+$conexao=mysql_connect($hostname,$user_db,$senha_db);mysql_select_db($banco_dados) or die("Erro na conexão com o banco de dados");
 
-$conexao=mysql_connect($hostname,$user_db,$senha_db);
-mysql_select_db($banco_dados) or die("Erro na conexão com o banco de dados");
+ if(!$conexao)
+     {
+  echo "Erro na conexão com o banco de dados";exit; 
+    } 
+else{
+   echo "Conexão bem sucedida";
+    }
 mysql_close();
 ?>
