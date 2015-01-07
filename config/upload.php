fe4b1f4bc5cc21baa238ba8a7a8f8f6c123f4dcb
Marcos Barbosa <bob.barbosa2@gmail.com>
2015-01-07 16:02:41 -0300

    Update upload.php

---
+++ config/upload.php
@@ -0,0 +1,43 @@
+<?php
+$_UP['pasta'] = 'diretodio';
+
+$_UP['tamanho'] = 1024 * 1024 * 2; // 2Mb
+
+$_UP['extensoes'] = array('jpg', 'png', 'gif');
+
+$_UP['renomeia'] = true;
+
+
+$extensao = strtolower(end(explode('.', $_FILES['img']['name'])));
+if (array_search($extensao, $_UP['extensoes']) === false) {
+	header("location:[url do redirecionamento]");
+	exit;
+}
+
+else if ($_UP['tamanho'] < $_FILES['img']['size']) {
+header("location:[url do redirecionamento]");
+exit;
+}
+
+
+else {
+
+if ($_UP['renomeia'] == true) {
+
+$nome_final = time().'.jpg';
+} else {
+
+$nome_final = $_FILES['img']['name'];
+}
+
+if (move_uploaded_file($_FILES['img']['tmp_name'], $_UP['pasta'] . $nome_final)) {
+
+} else {
+
+header("location:[url do redirecionamento]");
+exit;
+}
+
+}
+
+?>
